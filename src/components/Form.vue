1e94c7b093d899ac257be25e4b7ff8833ca83b24
AlexandruSerban142 <alex.serban142@gamil.com>
2019-12-08 20:30:04 +0200

    bug currentDay

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -320,7 +320,7 @@ export default {
       const date = new Date();
       const year = date.getFullYear();
       const month = date.getMonth() + 1;
-      const day = (date.getDay() + nrSkipDays + 1).toString().padStart(2, 0);
+      const day = (date.getDate() + nrSkipDays).toString().padStart(2, 0);
       return `${year}-${month}-${day}`;
     },
 
