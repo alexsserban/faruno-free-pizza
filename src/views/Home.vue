9618f938aa66b2c931b754aa982187ac8769f36f
AlexandruSerban142 <alex.serban142@gamil.com>
2019-12-09 13:44:28 +0200

    Update Menu & Welcome Message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,8 @@
         <div class="flex flex-col items-center justify-center w-2/3">
           <img class="h-24 my-10 mb-8" src="../assets/logo.png" alt="" />
           <p class="md:w-3/4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
+            Cu ocazia deschiderii noului local, poti completa formularul si
+            primesti de la noi o pizza gratis.
           </p>
 
           <div class="flex flex-wrap justify-center mt-8 md:w-full ">
